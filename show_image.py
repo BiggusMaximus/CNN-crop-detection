0ee1d6a85d53c69a00410dc57d3376c743065305
BiggusMaximus <annastyabagas164@gmail.com>
2023-04-30 13:13:23 +0700

    ganti ke opencv

--- show_image.py
+++ show_image.py
@@ -1,6 +1,6 @@
 import sys  # to access the system
 import cv2
-img = cv2.imread("NLB.jpg", cv2.IMREAD_ANYCOLOR)
+img = cv2.imread("NLB.JPG", cv2.IMREAD_ANYCOLOR)
 
 if img is not None and img.shape[0] > 0 and img.shape[1] > 0:
     cv2.imshow('Image', img)
@@ -8,4 +8,3 @@ if img is not None and img.shape[0] > 0 and img.shape[1] > 0:
     cv2.destroyAllWindows()
 else:
     print('Image not found or has invalid dimensions')
-
